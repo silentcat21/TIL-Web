c62222c1729e939813ef4bafa23e73d63eec27c0
silentcat21 <86968099+silentcat21@users.noreply.github.com>
2022-01-05 00:28:04 +0900

    2022.01.05

---
+++ Practice/language/app5.js
@@ -0,0 +1,12 @@
+const age = parseInt(prompt("Age?"))
+
+
+
+console.log(age);
+
+if(isNaN(age)){
+    console.log("please write a number")
+    //condition === true
+}else{
+
+}
---
+++ Practice/language/main.js
@@ -0,0 +1,3 @@
+const title = document.getElementById("title");
+
+console.dir(title);
