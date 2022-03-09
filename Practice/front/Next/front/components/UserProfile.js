3e31fbca3ce59707fe883feb6441e815da93f9fc
silentcat21 <86968099+silentcat21@users.noreply.github.com>
2022-03-09 20:37:18 +0900

    2022.03.09

--- Practice/front/Next/front/components/UserProfile.js
+++ Practice/front/Next/front/components/UserProfile.js
@@ -3,13 +3,18 @@ import {Button, Card} from 'antd';
 import Avatar from 'antd/lib/avatar/avatar';
 
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { logoutAction } from '../reducers';
 
 //style
 
-const UserProfile = ({ setIsLoggedIn }) => {
+const UserProfile = () => {
+    const dispatch = useDispatch();
+
     const onLogOut = useCallback(() => {
-        setIsLoggedIn(false);
-    }, []);
+        dispatch(logoutAction());
+    }, [])
+
     return (
         <>
             <Card 
--- Practice/front/Next/front/pages/about/about.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const About = () => {
-    return (
-        <div>무엇인가</div>
-    );
-}
-
-export default About;
